a12b429543ef316a871093b27c810626bc392614
feedme2 <pham.alan10@gmail.com>
2015-02-14 17:09:51 -0800

    changed everything to what Kevin had

--- src/Event.java
+++ src/Event.java
@@ -1,20 +1,35 @@
 
 public class Event {
 	
-	/*
-	 * Variables.
-	 */
-	
-	// The time when the event occurs.
 	public double eventTime;
-	
-	// True if it is arrival event, False if it is departure.
-	public double eventType;
-	
-	// Point to the next event.
+	public double serviceTime;
+	public double eventTime_arrival;
+	public double eventTime_departure;
+	public char eventType;
 	public Event next;
-	
-	// Previous event.
 	public Event previous;
 	
+	// Default Constructor
+    public Event() {
+    	eventTime = 0;
+    	serviceTime = 0;
+        eventTime_arrival = 0;
+        eventTime_departure = 0;
+        eventType = 'X';
+    }
+    
+    // Constructor.
+    public Event(double time, double service, char type) {
+    	eventTime = time;
+    	serviceTime = service;
+    	eventType = type;
+    }
+
+    // print the event
+    public void print() {
+        System.out.print("eventTime is: " + this.eventTime + ". serviceTime is " + this.serviceTime + " For event: " + this.eventType
+        );
+    }
+
+
 }
--- src/main.java
+++ src/main.java
@@ -1,13 +1,45 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-	/*
-	 * Global Variables.
-	 */
 	
-	// This is the maximum number of packets the can be held in queue.
-	private static int MAXBUFFER;
+	// GLOBAL VARIABLES
+	private static int MAXBUFFER; 	// max buffer
+    private static int length; 		// current length of the queue
+    private static double lambda;   // Arrival rate
+    private static double mu;       //Service rate
+    private static double rate;		// Rate variable for Negative Exponential Distribution
+	private static double time;
 	
-	public static void main(){
+	
+	// negative_exponentially_distributed_time
+    public static double negative_exponentially_distributed_time(double rate) {
+        double u = Math.random();
+        return ((-1/rate) * Math.log(1-u));
+    }
+	
+	// MAIN **************************************************************
+	public static void main(String[] args) {
+		
+		time = 0;
+        mu = 1;
+        lambda = rate = 0.1;
+		
+		// list
+		DoublyLinkedList list = new DoublyLinkedList();
+		
+		// queue
+		Queue<Event> queue = new LinkedList<Event>();
+		
+		//
+        Event first = new Event();
+        	first.eventTime = time + negative_exponentially_distributed_time(lambda);
+        	first.serviceTime = negative_exponentially_distributed_time(mu);
+        	first.eventType = 'A';
+        
+        list.add(first);
+        first.print();
 		
 	}
 }
